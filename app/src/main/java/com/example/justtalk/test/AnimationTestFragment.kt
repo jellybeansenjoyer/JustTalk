4128e0ad91b3ed146c0e5d3ed97f53e3327e97de
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-03-27 17:02:52 +0530

    Animation using ValueAnimator and ObjectAnimator test

--- app/src/main/java/com/example/justtalk/test/AnimationTestFragment.kt
+++ app/src/main/java/com/example/justtalk/test/AnimationTestFragment.kt
@@ -3,6 +3,7 @@ package com.example.justtalk.test
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import android.view.animation.AlphaAnimation
 import androidx.databinding.DataBindingUtil
 import com.example.justtalk.R
 import com.example.justtalk.databinding.FragmentAnimationTestBinding
+import java.time.temporal.ValueRange
 
 class AnimationTestFragment : Fragment() {
     lateinit private var mBinding : FragmentAnimationTestBinding
@@ -27,7 +29,57 @@ class AnimationTestFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mBinding.set.setOnClickListener {
+            when{
+                mBinding.checkBox.isChecked ->{
+                    objectAnim()
+                }
+                else->{
+                    valueAnim()
+                }
+            }
+        }
 
+    }
+
+    fun valueAnim(){
+        val alphaAnim = ValueAnimator.ofFloat(mBinding.alpha.alpha,0f).apply {
+            duration = 500
+            addUpdateListener {
+                mBinding.alpha.alpha = it.animatedValue as Float
+            }
+            repeatCount = 1
+            repeatMode = ValueAnimator.REVERSE
+        }
+
+        val transAnim = ValueAnimator.ofFloat(mBinding.trans.translationX,800f).apply {
+            duration = 200
+            addUpdateListener {
+                mBinding.trans.translationX = it.animatedValue as Float
+            }
+            repeatCount = 1
+            repeatMode = ValueAnimator.REVERSE
+        }
+
+        val rotateAnim = ValueAnimator.ofFloat(mBinding.rotate.rotation,360f).apply{
+            duration = 200
+            addUpdateListener {
+                mBinding.rotate.rotation = it.animatedValue as Float
+            }
+            repeatCount = 1
+            repeatMode = ValueAnimator.REVERSE
+        }
+        mBinding.trans.setOnClickListener {
+            transAnim.start()
+        }
+        mBinding.alpha.setOnClickListener {
+            alphaAnim.start()
+        }
+        mBinding.rotate.setOnClickListener {
+            rotateAnim.start()
+        }
+    }
+    fun objectAnim(){
         val alphaAnim = ObjectAnimator.ofFloat(mBinding.alpha,View.ALPHA,0f).apply {
             duration = 1000
             repeatCount = 1
@@ -45,10 +97,7 @@ class AnimationTestFragment : Fragment() {
             repeatCount = 1
             repeatMode = ObjectAnimator.REVERSE
         }
-        val animSet = AnimatorSet().apply {
-            play(alphaAnim).after(transAnim)
-            play(transAnim).after(rotatAnim)
-        }
+
         mBinding.alpha.setOnClickListener {
             alphaAnim.start()
         }
@@ -57,9 +106,6 @@ class AnimationTestFragment : Fragment() {
         }
         mBinding.rotate.setOnClickListener {
             rotatAnim.start()
-        }
-        mBinding.scale.setOnClickListener {
-            animSet.start()
         }
     }
 }
